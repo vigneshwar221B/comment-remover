0aa3df3c64b1e6d9cf67cdbfc0c5adcde570724b
vigneshwar221B <vigneshwar221b@gmail.com>
2019-04-14 00:43:05 +0530

    initial commit

---
+++ script.py
@@ -0,0 +1,24 @@
+import re
+import sys
+
+def del_comments(file_name):
+    new_str = ""
+    # to remove the single line comments
+    with open(file_name,'r') as f:
+        new_str = re.sub('(?<!\")(\/\/[\s\S]*?[\s])(?!\")', '', f.read(), flags=re.S)
+    with open(file_name, 'w') as f:
+        f.write(new_str)
+    # to remove the multi line comments
+    with open(file_name, 'r') as f:
+        new_str = re.sub('(?<!\")(\/\*[\s\S]*?\/)(?!\")', '', f.read(), flags=re.S)
+    with open(file_name, 'w') as f:
+        f.write(new_str)
+
+if len(sys.argv)>1:
+    print("cmd")
+    for filename in sys.argv[1:]:
+        del_comments(filename)
+else:
+    print("not cmd")
+    name = input("enter the filename: ")
+    del_comments(name)
